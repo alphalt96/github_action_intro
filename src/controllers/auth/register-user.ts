aed540c0f63389614be472ad73cca490ecd7b81a
alphalt96 <ngoquocanh22@gmail.com>
2022-03-01 19:18:32 +0700

    make some change

--- src/controllers/auth/register-user.ts
+++ src/controllers/auth/register-user.ts
@@ -5,7 +5,7 @@ export async function registerUser(request: Request, response: Response) {
     const requestBody = request.body;
     const serviceResponse = await authService.registerUser(requestBody);
 
-    console.log('Service response1', serviceResponse)
+    console.log('Service response', serviceResponse)
 
     return response.status(200).json({
         message: "success"
